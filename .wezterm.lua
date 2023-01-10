d9edc81f76b8542df070891eccd06f06215edbd4
nishigori <nishigori.tak@gmail.com>
2023-01-10 11:35:44 +0900

    [wezterm] few settins

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,9 +2,14 @@ local wezterm = require "wezterm"
 local act = wezterm.action
 
 -- Change this to match your heuristics for whether the executable path is an editor
-function is_an_editor(name)
    return name:find("nvim")
 end
+
+local custom = wezterm.color.get_builtin_schemes()['Catppuccin Latte']
+--custom.background = "#000000"
+custom.tab_bar.background = "#040404"
+custom.tab_bar.inactive_tab.bg_color = "#0f0f0f"
+custom.tab_bar.new_tab.bg_color = "#080808"
 
 -- for nvim
 local function emulate_command_shift(key)
@@ -34,8 +39,13 @@ return {
   --use_ime = false, -- awaiting fixed https://github.com/wez/wezterm/issues/2771
   --debug_key_events = true,
   font = wezterm.font "Hack Nerd Font",
-  font_size = 23,
-  color_scheme = "Raycast_Light",  -- "Google (light) (terminal.sexy)", "GoogleDark (Gogh)", "GoogleDark (Gogh)", "GitHub Dark"
+  font_size = 22,
+  line_height = 1.1,
+  color_schemes = {
+    ['OLEDppuccin'] = custom,
+  },
+  color_scheme = "OLEDppuccin", -- or Mocha, Macchiato, Frappe, Latte
+  --color_scheme = "Google (light) (terminal.sexy)",  -- "Raycast_Light", "Google (light) (terminal.sexy)", "GoogleDark (Gogh)", "GoogleDark (Gogh)", "GitHub Dark"
   colors = {
     -- https://docs.rs/palette/0.4.1/palette/named/index.html#constants
     cursor_bg = 'deeppink',
