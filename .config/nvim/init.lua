11af147c011af73066a74457206843169d987a64
nishigori <nishigori.tak@gmail.com>
2025-02-10 00:17:17 +0900

    [nvim] fix :checkhealth

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -13,6 +13,9 @@ g["loaded_tar"] = 1
 g["loaded_zipPlugin"] = 1
 g["loaded_zip"] = 1
 g["loaded_netrwPlugin"] = 1
+g["loaded_node_provider"] = 0
+g["loaded_perl_provider"] = 0
+g["loaded_ruby_provider"] = 0
 
 -- Settings
 local opt = vim.opt
