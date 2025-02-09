8017a726ac24138dc9e0fa979c11b1143e4368e0
nishigori <nishigori.tak@gmail.com>
2025-02-10 00:18:22 +0900

    [nvim] update lazy.nvim settings almost official latest

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -68,24 +68,7 @@ if not vim.g.vscode then
   --cmd([[set grepformat=%f:%l:%c:%m]])
 end
 
-g.mapleader = " "
-
--- Plugins
--- https://github.com/folke/lazy.nvim#-installation
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup("plugins")
+require("config.lazy")
 
 -- KeyMap
 require('keymaps')
